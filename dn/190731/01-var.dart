511ad4210889fde7fb8078daa0b69d9dcbcdc454
hwayn <32152138+winningH@users.noreply.github.com>
2019-07-31 23:06:03 +0800

    dart learn

--- dn/190731/01-var.dart
+++ dn/190731/01-var.dart
@@ -1,11 +1,11 @@
-main(List<Stirng> args) {
+main(List<String> args) {
   var a;
   // 变量，没有初始值，那么它可以是任何类型
   a = 'huang';
   a = 123;
   a = true;
-  a = { 'key': 'value' }
-  a = ['123', 'abc']
+  a = { 'key': 'value' };
+  a = ['123', 'abc'];
   print(a);
 
   Object b = 'stirng';
@@ -13,18 +13,18 @@ main(List<Stirng> args) {
   b = { 'key': 'value'};
   print(b);
 
-  dynamic c = 'sring';
+  dynamic c = 'string';
   c = 123;
 
-  var d = 123;
-  d = 'stirng';
+  // var d = 123;
+  // d = 'stirng';  //不允许
 
   // 使用var声明的时候，如果没有初始值，后边可以为任意类型。
   // 如果有初始值，则类型被锁定
 
-  String str = '123'; // utf-16 的 字符组成
-  bool b;     // true 和 false， 都是编译时常量
-  num c;      // dart 中的 数值类型
+  // String str = '123'; // utf-16 的 字符组成
+  // bool b;     // true 和 false， 都是编译时常量
+  // num c;      // dart 中的 数值类型
   // int double
   // 使用强类型声明的时候，类型被锁定
 
@@ -32,8 +32,10 @@ main(List<Stirng> args) {
   // + - * / %
 
   // 字符串模板
-  String s = 'hello world'
-  String s1 = '输入一个大写的${s.toUpperCase()}'
+  String s = 'hello world';
+  String s1 = '输入一个大写的${s.toUpperCase()}';
+  print(s1);
 
   var s2 = 'hello' + '' + 'world';
+  print(s2);
 }
