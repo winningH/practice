884fead70ee3f226d25ada99045433662162afed
hwayn <32152138+winningH@users.noreply.github.com>
2019-08-04 20:04:50 +0800

    dart list&map

--- dn/2019/190731/03-list&map.dart
+++ dn/2019/190731/03-list&map.dart
@@ -3,12 +3,13 @@ main(List<String> args) {
   // 对象 map
 
   var arr = ['jack', 'amy', 'tony'];
+  print(arr);
   var arr2 = List.of([1, 2, 3]); // 构造函数
 
   arr2.add(500);
   arr2.forEach((v) => print('$v'));
 
-  var map1 = {'name': 'Tom', 'sex': 'male'};  // 字面量
+  // var map1 = {'name': 'Tom', 'sex': 'male'};  // 字面量
   var map2 = new Map(); // dart 在2.0以后，new 可以省略
   map2['name'] = 'Jack';
   map2['sex'] = 'famale';
@@ -18,4 +19,6 @@ main(List<String> args) {
   // arr2 的类型是 List<int>
   List<int> ls = [1, 2, 3];
   List<String> ls2 = ['1', '2', '3'];
+  print(ls);
+  print(ls2);
 }
