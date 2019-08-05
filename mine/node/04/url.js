05dd291e7885e76feb31971e4d07cac7548c1e71
hwayn <hwayning@gmail.com>
2019-08-05 09:14:45 +0800

    add comment

--- mine/node/04/url.js
+++ mine/node/04/url.js
@@ -2,6 +2,7 @@ const http = require('http');
 const urlLib = require('url');
 
 http.createServer(function (req, res) {
+  // true 将obj.query 转成 objecct
   var obj = urlLib.parse(req.url, true);
   console.log(obj);
   res.end('finish');
